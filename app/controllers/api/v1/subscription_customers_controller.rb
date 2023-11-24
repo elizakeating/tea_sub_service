fe7133d6f32aff9e9db777fe0310d7ba48b65158
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 15:16:41 -0700

    feat: add basic error catching and responses to subscription customers controller

--- app/controllers/api/v1/subscription_customers_controller.rb
+++ app/controllers/api/v1/subscription_customers_controller.rb
@@ -1,29 +1,31 @@
 class Api::V1::SubscriptionCustomersController < ApplicationController
   def index
-    customer = Customer.find(params[:customer_id])
-
-    render json: SubscriptionSerializer.new(customer.subscriptions)
-  end
+    begin
+      customer = Customer.find(params[:customer_id])
+      render json: SubscriptionSerializer.new(customer.subscriptions)
+    rescue ActiveRecord::RecordNotFound => exception
+      render json: {error: exception.message}
+    end
+  end 
 
   def create
-    Subscription.find(params[:subscription_id]).update_column(:status, 1)
-
-    subscribed = SubscriptionCustomer.new(subscription_id: params[:subscription_id], customer_id: params[:customer_id])
+    subscribed = SubscriptionCustomer.new(subscription_id: params[:subscription_id], customer_id: params[:customer_id], status: 1)
 
     if subscribed.save
       render json: {success: "Customer has been successfully subscribed."}
+    else
+      render json: {error: "Customer was not able to be subscribed. Please make sure the customer or subscription id is correct."}
     end
   end
 
   def update
-    change_status = Subscription.find(params[:subscription_id]).update_column(:status, 0)
-    
     subscribed = SubscriptionCustomer.find_by(subscription_id: params[:subscription_id], customer_id: params[:customer_id])
 
     if subscribed
-      if change_status
-        render json: {success: "Customer has been successfully unsubscribed."}
-      end
+      subscribed.update_column(:status, 0)
+      render json: {success: "Customer has been successfully unsubscribed."}
+    else
+      render json: {error: "Customer was not able to be unsubscribed. Please make sure the customer or subscription id is correct."}
     end
   end
 end
