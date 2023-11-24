5657eab15a299a476c689018b78b01f7e382b7b8
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 13:17:50 -0700

    feat: add destroy method exit

--- app/controllers/api/v1/subscription_customers_controller.rb
+++ app/controllers/api/v1/subscription_customers_controller.rb
@@ -8,4 +8,16 @@ class Api::V1::SubscriptionCustomersController < ApplicationController
       render json: {success: "Customer has been successfully subscribed."}
     end
   end
+
+  def destroy
+    change_status = Subscription.find(params[:subscription_id]).update_column(:status, 0)
+    
+    subscribed = SubscriptionCustomer.find_by(subscription_id: params[:subscription_id], customer_id: params[:customer_id])
+
+    if subscribed
+      if change_status
+        render json: {success: "Customer has been successfully unsubscribed."}
+      end
+    end
+  end
 end
