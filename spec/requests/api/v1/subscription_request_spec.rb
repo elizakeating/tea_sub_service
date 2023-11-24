1962eb58e384af31ea564f9b62e047457b9afea4
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 13:17:07 -0700

    test: add test for cancelling subscription

--- spec/requests/api/v1/subscription_request_spec.rb
+++ spec/requests/api/v1/subscription_request_spec.rb
@@ -46,4 +46,23 @@ RSpec.describe "Subscription Request" do
     expect(@customer_1.subscriptions.first.status).to eq("active")
     expect(@customer_1.subscriptions.first.frequency).to eq("2 weeks")
   end
+
+  it "sends message showing that customer has been sucessfully unsubscribed from a tea subscription" do
+    SubscriptionCustomer.create(customer_id: @customer_1.id, subscription_id: @subscription.id)
+
+    delete "/api/v1/customers/#{@customer_1.id}/subscriptions/#{@subscription.id}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json).to eq({success: "Customer has been successfully unsubscribed."})
+
+    expect(@customer_1.subscriptions.count).to eq(1)
+    expect(@customer_1.subscriptions.first).to be_a(Subscription)
+    expect(@customer_1.subscriptions.first.title).to eq("Minty Delight")
+    expect(@customer_1.subscriptions.first.price).to eq(25.0)
+    expect(@customer_1.subscriptions.first.status).to eq("cancelled")
+    expect(@customer_1.subscriptions.first.frequency).to eq("2 weeks")
+  end
 end
