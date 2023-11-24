f1b07351bd9c937c7f3688ce09873a7c438bb4bd
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 15:15:30 -0700

    feat: remove status from subscription and add it to subscription_customer

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,8 +3,6 @@ class Subscription < ApplicationRecord
   validates :price, presence: true
   validates :frequency, presence: true
 
-  enum status: { cancelled: 0, active: 1 }
-
   has_many :subscription_customers
   has_many :customers, through: :subscription_customers
   has_many :tea_subscriptions
--- app/models/subscription_customer.rb
+++ app/models/subscription_customer.rb
@@ -1,4 +1,6 @@
 class SubscriptionCustomer < ApplicationRecord
   belongs_to :subscription
   belongs_to :customer
+
+  enum status: { cancelled: 0, active: 1 }
 end
