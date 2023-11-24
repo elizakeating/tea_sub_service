6dd592ca8f30bfde28c5c9fdf2819923c11f819d
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 13:03:05 -0700

    feat: add route to subscribe customer to a tea subscription

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      post "/customers/:customer_id/subscriptions/:subscription_id", to: "subscription_customers#create"
+    end
+  end
 end
