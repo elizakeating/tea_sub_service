5961bc9fe83f531220a7fa1d494002cecf01c079
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 13:41:55 -0700

    test: add test for subscription serializer

---
+++ spec/serializers/subscription_serializer_spec.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.describe SubscriptionSerializer do
+  it "should return a serialized subscription" do
+    subscription = Subscription.create(
+      title: "Fruity Fun",
+      price: 45.00,
+      frequency: "1 month"
+    )
+
+    serialized_data = SubscriptionSerializer.new(subscription).serializable_hash
+
+    expect(serialized_data).to eq({
+      data: {
+        id: subscription.id.to_s,
+        type: :subscription,
+        attributes: {
+          title: subscription.title,
+          price: subscription.price,
+          status: subscription.status,
+          frequency: subscription.frequency
+        }
+      }
+    })
+  end
+end
