db133d3e73dd25df4286751cd20ee168d1aab3fa
Eliza Keating <ek.elizakeating@gmail.com>
2023-11-24 09:56:48 -0700

    test: model relationship and validation testing

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe Subscription, type: :model do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :price }
+    it { should validate_presence_of :status }
+    it { should validate_presence_of :frequency }
+  end
+
+  describe "relationships" do
+    it { should have_many :subscription_customers }
+    it { should have_many(:customers).through(:subscription_customers) }
+    it { should have_many :tea_subscriptions }
+    it { should have_many(:teas).through(:tea_subscriptions) }
+  end
+end
